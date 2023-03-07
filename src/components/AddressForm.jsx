6c899a8cfc08edd07b8746a2df53a8c44820f3c0
DavidDGamble <daviddgamble@gmail.com>
2023-03-07 11:11:32 -0800

    Merge checkout into main.

--- src/components/AddressForm.jsx
+++ src/components/AddressForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import PropTypes from 'prop-types'
-import { json, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import * as u from './../utilities/utilities'
 
 function AddressForm(props) {
@@ -18,7 +18,6 @@ function AddressForm(props) {
 
   const navigate = useNavigate()
   useEffect(() => {
-    console.log(viewCheckout)
     if (viewCheckout) navigate('/checkout', { replace: true })
   }, [viewCheckout])
   
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import PropTypes from "prop-types";
 
 function Header(props) {
-  let welcomeMsg = null;
-  if (props.currUserDisplay != null) {
-    welcomeMsg = `Welcome back, ${props.currUserDisplay}!`
-  } else {
-    welcomeMsg = "Welcome!";
-  }
+  // let welcomeMsg = null;
+  // if (props.currUserDisplay != null) {
+  //   welcomeMsg = `Welcome back, ${props.currUserDisplay}!`
+  // } else {
+  //   welcomeMsg = "Welcome!";
+  // }
 
   return(
     <div className="header">
